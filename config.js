6936db28bc065faae2331c0c16415b08fc9c0189
Vaishnavi2004 <78804893+Vaishnavi2004@users.noreply.github.com>
2021-12-03 16:54:04 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCVhoma5DGtq3jNwCosUIPvX3Iprf8QCqQ",
+  authDomain: "story-telling-app-56d76.firebaseapp.com",
+  databaseURL: "https://story-telling-app-56d76-default-rtdb.firebaseio.com",
+  projectId: "story-telling-app-56d76",
+  storageBucket: "story-telling-app-56d76.appspot.com",
+  messagingSenderId: "496214725000",
+  appId: "1:496214725000:web:d7b3d3b838297111aa2a3d"
+};
